fa66c17d8ae67bc639685fb476596b54a784fdca
KY64 <taurus99@protonmail.com>
2020-10-12 20:12:40 +0700

    Revert "Change format to JSX for better linter"
    This reverts commit 157f6642602a0fd353673a37dc1af07e3fe889d1.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Content from './components/simplecontrol';
+import {host} from "./utils";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    
+  }
+  render() {
+    console.log(host)
+      return (
+        <main>
+        {!host 
+    ? <h1 style={{textAlign:"center", marginTop:"5em"}}> Please define REACT_APP_API_HOSTNAME Environment Variable </h1>
+    : <div>
+        <Navbar />
+        <Content />
+      </div>}
+        </main>
+    );
+  }
+}
+
+export default App;
