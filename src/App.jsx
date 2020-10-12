d0e8ea8eb677a8b69ebfcd6b126af6ad3c7d90d9
KY64 <taurus99@protonmail.com>
2020-10-13 04:13:57 +0700

    Add tabs to navigate between view

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Content from "./components/Content";
 import Control from "./pages/controller";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Layout from "./components/Layout";
-import Navbar from "./components/Navbar";
-import React from "react";
+import Logs from "./pages/Logs";
+import React, {useState} from "react";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
 import custom from "./utility/theme";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   createMuiTheme,
   ThemeProvider,
@@ -20,6 +24,27 @@ const App = () => {
     ...custom,
   });
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
+  const useStyles = makeStyles((theme) => ({
+    heading: {
+      margin: "0 3rem",
+      [theme.breakpoints.down("xs")]: {
+        margin: "0 2rem",
+      },
+    },
+    nav: {
+      textAlign: "center",
+      marginTop: "120px",
+      [theme.breakpoints.down("xs")]: {
+        marginTop: "30px",
+      },
+    },
+  }));
+
+  const [value, setValue] = useState(0);
+  const classes = useStyles();
+
+  const changeValue = (event, newValue) => setValue(newValue);
 
   return (
     <ThemeProvider theme={theme}>
@@ -39,12 +64,20 @@ const App = () => {
           )}
         </>
       ) : (
-        <>
-          <Navbar />
-          <Container maxWidth={matches ? "sm" : "xs"}>
-            <Control />
-          </Container>
-        </>
+        <Container maxWidth={matches ? "sm" : "xs"}>
+          <Box className={classes.nav}>
+            <Tabs value={value} onChange={changeValue} centered>
+              <Tab label="IoT Control" className={classes.heading} />
+              <Tab label="Logs" className={classes.heading} />
+            </Tabs>
+          </Box>
+          <Content value={value} index={0}>
+          <Control />
+        </Content>
+        <Content value={value} index={1}>
+          <Logs />
+        </Content>
+        </Container>
       )}
     </ThemeProvider>
   );
