71eee734db327f77c57ff31871ba4c425091ea62
KY64 <taurus99@protonmail.com>
2020-10-12 20:10:41 +0700

    Change component file format to JSX for better linter

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,27 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
 export default function ButtonAppBar() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6">
             IoT Control
           </Typography>
         </Toolbar>
