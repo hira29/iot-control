ed672c77644be66f7963a54615d33fee83b4b54a
KY64 <taurus99@protonmail.com>
2020-10-13 02:48:36 +0700

    Redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,50 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Content from "./components/simplecontrol";
-import Layout from "./components/Layout";
-import { createMuiTheme, ThemeProvider, useTheme } from "@material-ui/core/styles";
-import { host } from "./utility/utils";
+import Control from "./pages/controller";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Layout from "./components/Layout";
+import Navbar from "./components/Navbar";
+import React from "react";
 import custom from "./utility/theme";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Container from "@material-ui/core/Container";
+import {
+  createMuiTheme,
+  ThemeProvider,
+  useTheme,
+} from "@material-ui/core/styles";
+import { host } from "./utility/utils";
 
 const App = () => {
-let theme = useTheme();
-theme = createMuiTheme({
-  ...theme,
-  ...custom
-})
+  let theme = useTheme();
+  theme = createMuiTheme({
+    ...theme,
+    ...custom,
+  });
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       {!host ? (
-        <h1 style={{ textAlign: "center", marginTop: "5em" }}>
-          {" "}
-          Please define REACT_APP_API_HOSTNAME Environment Variable{" "}
-        </h1>
+        <>
+          {matches ? (
+            <h2 style={{ textAlign: "center", marginTop: "5em" }}>
+              {" "}
+              Please define REACT_APP_API_HOSTNAME Environment Variable{" "}
+            </h2>
+          ) : (
+            <h5 style={{ textAlign: "center", marginTop: "5em" }}>
+              {" "}
+              Please define REACT_APP_API_HOSTNAME Environment Variable{" "}
+            </h5>
+          )}
+        </>
       ) : (
-        <Layout>
+        <>
           <Navbar />
-          <Content />
-        </Layout>
+          <Container maxWidth={matches ? "sm" : "xs"}>
+            <Control />
+          </Container>
+        </>
       )}
     </ThemeProvider>
   );
