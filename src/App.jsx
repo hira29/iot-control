f4cd99c8a557ad956e680fd52b558782e360afb7
KY64 <taurus99@protonmail.com>
2020-10-12 22:12:55 +0700

    Add custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
-import Navbar from './components/navbar';
-import Content from './components/simplecontrol';
-import {host} from "./utils";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Content from "./components/simplecontrol";
+import { createMuiTheme, ThemeProvider, useTheme } from "@material-ui/core/styles";
+import { host } from "./utility/utils";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import custom from "./utility/theme";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    
-  }
-  render() {
-    console.log(host)
-      return (
-        <main>
-        {!host 
-    ? <h1 style={{textAlign:"center", marginTop:"5em"}}> Please define REACT_APP_API_HOSTNAME Environment Variable </h1>
-    : <div>
-        <Navbar />
-        <Content />
-      </div>}
-        </main>
-    );
-  }
-}
+const App = () => {
+let theme = useTheme();
+theme = createMuiTheme({
+  ...theme,
+  ...custom
+})
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {!host ? (
+        <h1 style={{ textAlign: "center", marginTop: "5em" }}>
+          {" "}
+          Please define REACT_APP_API_HOSTNAME Environment Variable{" "}
+        </h1>
+      ) : (
+        <div>
+          <Navbar />
+          <Content />
+        </div>
+      )}
+    </ThemeProvider>
+  );
+};
 
 export default App;
