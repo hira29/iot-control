693577726270ef107fb07624dc5a9ef6487c57a7
KY64 <taurus99@protonmail.com>
2020-10-13 02:46:13 +0700

    Redesign navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,10 @@ import Tab from "@material-ui/core/Tab";
 export default function ButtonAppBar() {
   const useStyles = makeStyles(theme => ({
     heading : {
-      fontWeight: 400,
-      display: "inline-block",
-      margin: "0 3rem"
+      margin: "0 3rem",
+      [theme.breakpoints.down("xs")]: {
+        margin: "0 2rem"
+      }
     },
     nav : {
       textAlign: "center",
@@ -29,8 +30,8 @@ export default function ButtonAppBar() {
   return (
         <Box className={classes.nav}>
           <Tabs value={value} onChange={changeValue} centered>
-            <Tab label="IoT Control" />
-            <Tab label="Logs" />
+            <Tab label="IoT Control" className={classes.heading}/>
+            <Tab label="Logs" className={classes.heading}/>
           </Tabs>
         </Box>
   );
