e2e4e7456072dafc3a77ac355ca1bb97c92e7f5b
KY64 <taurus99@protonmail.com>
2020-12-28 17:51:11 +0700

    Add sensor value and get latest value periodically

--- src/pages/controller.jsx
+++ src/pages/controller.jsx
@@ -1,20 +1,67 @@
-import React from "react";
-import Chip from "@material-ui/core/Chip";
 import Box from "@material-ui/core/Box";
+import Chip from "@material-ui/core/Chip";
+import Grid from "@material-ui/core/Grid";
 import LED from "../components/LED";
+import React, {useEffect, useState} from "react"
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
 import { path, query } from "../utility/utils";
+import {makeStyles} from "@material-ui/core/styles";
 
+const useStyles = makeStyles(theme => ({
+  title : {
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "11pt",
+    }
+  },
+  text : {
+    fontWeight: 300,
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "13pt",
+    }
+  }
+})) 
+
+let val = 0;
 const Simplecontrol = () => {
+  const [sensorvalue, setSensorValue] = useState(0);
+  const classes = useStyles();
+  query.sensor.size = 1
+
+  useEffect(() => {
+    console.log("yo")
+  })
+
+  setInterval(() => {
+    axios({
+      method: "post",
+      headers: {
+        "content-type": "application/json"
+      },
+      url: path.log.sensor,
+      data: query.sensor
+    })
+      .then(data => {
+        setSensorValue(data.data.data.content[0].data)
+      })
+      .catch(err => console.error(err))
+  }, 1000)
 
   return (
     <Box marginTop="0.4em">
+      <Box textAlign="center">
+        <Typography variant="h6" className={classes.title}>
+          Sensor Value
+        </Typography>
+        <Typography variant="h4" className={classes.text}>{sensorvalue}</Typography>
+      </Box>
       <Chip
         label="LED 1"
         variant="outlined"
         color="primary"
         style={{ marginTop: "2em" }}
       />
-      <LED led={path.led1} log={query.led1}/>
+        <LED led={path.led1} log={query.led1}/>
       <Chip
         label="LED 2"
         variant="outlined"
